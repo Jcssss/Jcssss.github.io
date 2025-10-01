5fa58ed435d9b963ac775140c91b36af0ea68729
Justin Siu <justsiu545@gmail.com>
2025-10-01 00:01:28 -0700

    Update links to embed in title of projects rather than separate for formatting purposes

--- src/components/page-contents/ProjectsContent.js
+++ src/components/page-contents/ProjectsContent.js
@@ -7,32 +7,28 @@ const ProjectsContent = (props) => {
                 <h2 class="title">Projects:</h2>
                 <div className='experience content'>
                     <p class="main-content">
-                        <h4>Team Fight Tactics Team Planner</h4>
-                        <a href="https://github.com/Jcssss/TFTCompPlanner">https://github.com/Jcssss/TFTCompPlanner</a>
+                        <h4><a href="https://github.com/Jcssss/TFTCompPlanner">Team Fight Tactics Team Planner</a></h4>
                     </p>
                     <ul class="main-content">
                         <li>A responsive team planner for the video game Team Fight Tactics</li>
                     </ul>
                     <div></div>
                     <p class="main-content">
-                        <h4>Green Not Greed Database and UI</h4>
-                        <a href="https://github.com/Jcssss/GNG">https://github.com/Jcssss/GNG</a>
+                        <h4><a href="https://github.com/Jcssss/GNG">Green Not Greed Database and UI</a></h4>
                     </p>
                     <ul class="main-content">
                         <li>A relational database and text-based user interface for the company Green Not Greed.</li>
                     </ul>
                     <div></div>
                     <p class="main-content">
-                        <h4>Ray Tracer</h4>
-                        <a href="https://github.com/Jcssss/ray-tracer">https://github.com/Jcssss/ray-tracer</a>
+                        <h4><a href="https://github.com/Jcssss/ray-tracer">Ray Tracer</a></h4>
                     </p>
                     <ul class="main-content">
                         <li>A series of ray tracers built with increasing complexity with components including reflection, refraction, rasterization, and shadows.</li>
                     </ul>
                     <div></div>
                     <p class="main-content">
-                        <h4>Last Light</h4>
-                        <a href="https://github.com/Jcssss/LastLight">https://github.com/Jcssss/LastLight</a>
+                        <h4><a href="https://github.com/Jcssss/LastLight">Last Light</a></h4>
                     </p>
                     <ul class="main-content">
                         <li>A 2D platformer where you and your loyal companion, Pixie, must escape the darkness and monsters that haunts the lighthouse.</li>
